d072c3b4bb89d70bf158e86d67043ee80e3716ef
Allister William Gray Gardner <allister.w.g.gardner@gmail.com>
2022-04-20 11:36:04 +0100

    added 3 new fields to the User class

--- src/main/java/com/qa/intro_project/data/entity/User.java
+++ src/main/java/com/qa/intro_project/data/entity/User.java
@@ -1,7 +1,12 @@
 package com.qa.intro_project.data.entity;
 
+import java.util.Date;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
 
 // Spring has a 'spring-boot-starter-validation' module which transitively includes
@@ -30,6 +35,21 @@ public class User {
 	@NotBlank
 	@Size(min = 2, max = 16, message = "Username must have at least 2 characters, but no more than 16")
 	private String username;
+	
+	@NotNull
+	@NotBlank
+	@Email
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	@Past
+	private Date dob;
+	
+	@NotNull
+	@NotBlank
+	@PastOrPresent
+	private Date dateCreated;
 	
 	// TODO: Add 3 new fields to the User class, with appropriate validation annotations applied to each
 	
@@ -61,7 +81,7 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", date of birth=" + dob + ", date created=" + dateCreated + "]";
 	}
 	
 }
